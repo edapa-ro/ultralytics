ed83156b90f6fd3385d32084b38a8688b3937f11
AlfredPietraru <alfred.andrei@yahoo.com>
2025-08-09 10:13:17 +0300

    added variations to yolov5C2fGhost.yaml

--- pipeline.sh
+++ pipeline.sh
@@ -5,8 +5,8 @@
 YAML=$1
 SIZE=1024
 
-DATASET_YAML=ultralytics/cfg/datasets/downtest.yaml
-# DATASET_YAML=ultralytics/cfg/datasets/coco8.yaml
+# DATASET_YAML=ultralytics/cfg/datasets/downtest.yaml
+DATASET_YAML=ultralytics/cfg/datasets/coco8.yaml
 
 MODEL_NAME=$(basename $YAML .yaml)
 SAVE_MODEL_PATH=${MODEL_NAME}_saved_model
@@ -55,15 +55,30 @@ if [ ! $? -eq 0 ]; then
 fi
 
 mv ${MODEL_NAME}.onnx ${MODEL_NAME}_full_integer_quant_edgetpu.log  ${MODEL_NAME}_full_integer_quant_edgetpu.tflite ${SAVE_MODEL_PATH}
-mv runs/detect/train  ${SAVE_MODEL_PATH}
-mv ${SAVE_MODEL_PATH}/train/weights/best.pt ${SAVE_MODEL_PATH}/${MODEL}
-zip -r ${SAVE_MODEL_PATH}.zip ${SAVE_MODEL_PATH}
 deactivate
+
+mv runs/detect/train  ${SAVE_MODEL_PATH}
+rm -r ${SAVE_MODEL_PATH}/save_model.pb ${SAVE_MODEL_PATH}/fingerprint.pb ${SAVE_MODEL_PATH}/assets ${SAVE_MODEL_PATH}/variables ${SAVE_MODEL_PATH}/metada.yaml
+rm -r ${SAVE_MODEL_PATH}/train/*.png ${SAVE_MODEL_PATH}/train/*.jpg
+rm -r ${SAVE_MODEL_PATH}/half_channels_yolov5C2fGhost_float16.tflite ${SAVE_MODEL_PATH}/half_channels_yolov5C2fGhost_float32.tflite ${SAVE_MODEL_PATH}/half_channels_yolov5C2fGhost.onnx
+rm -r ${SAVE_MODEL_PATH}/train/weights/epoch*.pt
+mv ${SAVE_MODEL_PATH}/train/weights/best.pt ${SAVE_MODEL_PATH}/${MODEL}
+zip -r archive_${SAVE_MODEL_PATH}.zip ${SAVE_MODEL_PATH}
 
 
 source inference_venv/bin/activate
 python3 inference.py \
 --model ${SAVE_MODEL_PATH}/${MODEL_NAME}_full_integer_quant_edgetpu.tflite \
 --data ${DATASET_YAML}
+deactivate
 
+if [ ! $? -eq 0 ]; then
+    echo "Inference failed"
+    exit 1
+fi
+
+source venv/bin/activate
+python3 visualize.py \
+--data "./datasets/downtest/" \
+--labels "./res/labels/test/"
 deactivate
