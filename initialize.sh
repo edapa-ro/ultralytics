f1654aaedb4249b24163373e86c3e30e6cfcdf4c
AlfredPietraru <alfred.andrei@yahoo.com>
2025-08-09 10:27:52 +0300

    update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+apt-get update
 apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl zip
